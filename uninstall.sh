b5b65556c0022843b570c4b3c6431047b1cf63e2
bbangg <bbanggdev@gmail.com>
2022-08-20 09:17:33 +0300

    uninstall

---
+++ uninstall.sh
@@ -0,0 +1,8 @@
+find /home/clp/htdocs/app/files/templates/Admin/layout.html.twig -type f -exec sed -i 's/\/admin\/style-dark\.css/\/admin\/style\.css/g' {} \;
+find /home/clp/htdocs/app/files/templates/Admin/layout.html.twig -type f -exec sed -i 's/\/css\/style-dark\.css/\/css\/style\.css/g' {} \;
+find /home/clp/htdocs/app/files/templates/Admin/Partial/header.html.twig -type f -exec sed -i 's/logo-dark\.svg/logo\.svg/g' {} \;
+find /home/clp/htdocs/app/files/templates/Frontend/Login/layout.html.twig -type f -exec sed -i 's/\/css\/style-dark\.css/\/css\/style\.css/g' {} \;
+find /home/clp/htdocs/app/files/templates/Frontend/layout.html.twig -type f -exec sed -i 's/\/css\/style-dark\.css/\/css\/style\.css/g' {} \;
+find /home/clp/htdocs/app/files/templates/Frontend/Partial/header.html.twig -type f -exec sed -i 's/logo-dark\.svg/logo\.svg/g' {} \;
+find /home/clp/htdocs/app/files/templates/Frontend/Dashboard/index.html.twig -type f -exec sed -i 's/dashboard-dark\.css/dashboard\.css/g' {} \;
+find /home/clp/htdocs/app/files/templates/Frontend/Dashboard/index.html.twig -type f -exec sed -i 's/chart\: { backgroundColor: \"#353535\",/chart\: {/g' {} \;
